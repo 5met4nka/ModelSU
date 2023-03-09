99f12d25f846e4e70f4e89ef75fee4f86c6d0828
5met4nka <pavilich99@mail.ru>
2023-03-09 14:22:50 +0300

    подкинул код на котором работали на паре и удалил симулинк модель

--- lab3/main.m
+++ lab3/main.m
@@ -1,38 +1,121 @@
-figure('name', 'M=1 R=1', 'color','white');
-subplot(2,3,1);
-plot(flow(:,1),flow(:,2), '-k');
+clc, clear
+t = 0:0.01:1;
+u = [ones(size(t)); ones(size(t)); ones(size(t))];
+x0 = [0;0;0];
+[y,x] = nsim('fun_F', 'fun_G', t, u, x0, 'eiler');
+plot(t,x(1,:), 'k--', t, x(2,:), 'k', t, x(3,:), 'k:');
 grid on;
-grid minor;
-xlabel('t');
-ylabel('Flow');
-subplot(2,3,2);
-plot(i(:,1),i(:,2), '-k');
-xlabel('t');
-ylabel('iг');
-subplot(2,3,3);
-plot(w(:,1),w(:,2), '-k');
+title('График при номинальных значениях');
+legend('x_1(t)', 'x_2(t)', 'x_3(t)');
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t)); ones(size(t)); ones(size(t))];
+x0=[0;0;0];
+[y,x]=nsim('fun_F', 'fun_G', t, u, x0, 'eiler');
+plot(t,y(1,:),'k--',t,y(2,:),'k');
 grid on;
-grid minor;
-xlabel('t');
-ylabel('w');
-subplot(2,3,2);
+title('График при номинальных значениях');
+legend('y_1(t)', 'y_2(t)');
+
+
+clc, clear
+t= 0:0.01:1;
+u=[ones(size(t)); ones(size(t)); ones(size(t))];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(x(2,:),x(3,:),'k');
 grid on;
-grid minor;
-subplot(2,3,4);
-plot(flow(:,2),i(:,2), '-k');
-grid on;
-grid minor;
-xlabel('Flow');
-ylabel('iг');
-subplot(2,3,5);
-plot(flow(:,2),w(:,2), '-k');
-grid on; 
-grid minor;
-xlabel('Flow'); 
-ylabel('w')
-subplot(2,3,6);
-plot(i(:,2),w(:,2), '-k');
-grid on; 
-grid minor;
-xlabel('iг'); 
-ylabel('w');
+title('Фазовый портрет при номинальных значениях');
+legend('x_3(x_2)');
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t)); ones(size(t)); ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(t,x(1,:),'k--',t,x(2,:),'k',t,x(3,:),'k:'), grid on
+title('График при уменьшенном U_c на 20%')
+legend('x_1(t)','x_2(t)','x_3(t)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t));ones(size(t));ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(t,y(1,:),'k--',t,y(2,:),'k'), grid on
+title('График при уменьшенном U_c на 20%')
+legend('y_1(t)','y_2(t)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t));ones(size(t));ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(x(2,:),x(3,:),'k'), grid on
+title('Фазовый портрет при уменьшенном U_c на 20%')
+legend('x_3(x_2)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t));ones(size(t))*0.85;ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(t,x(1,:),'k--',t,x(2,:),'k',t,x(3,:),'k:'), grid on
+title('График при уменьшенном M_в на 15%, U_c на 20%')
+legend('x_1(t)','x_2(t)','x_3(t)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t));ones(size(t))*0.85;ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(t,y(1,:),'k--',t,y(2,:),'k'), grid on
+title('График при уменьшенном M_в на 15%, U_c на 20%')
+legend('y_1(t)','y_2(t)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t));ones(size(t))*0.85;ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(x(2,:),x(3,:),'k'), grid on
+title('Фазовый портрет при уменьшенном M_в на 15%, U_c на 20%')
+legend('x_3(x_2)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t))*0.8;ones(size(t))*0.85;ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(t,x(1,:),'k--',t,x(2,:),'k',t,x(3,:),'k:'), grid on
+title('График при уменьшенном M_в на 15%, U_c на 20%, U_в на 20%')
+legend('x_1(t)','x_2(t)','x_3(t)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t))*0.8;ones(size(t))*0.85;ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(t,y(1,:),'k--',t,y(2,:),'k'), grid on
+title('График при уменьшенном M_в на 15%, U_c на 20%, U_в на 20%')
+legend('y_1(t)','y_2(t)')
+
+
+clc, clear
+t=0:0.01:1;
+u=[ones(size(t))*0.8;ones(size(t))*0.85;ones(size(t))*0.8];
+x0=[0;0;0];
+[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
+plot(x(2,:),x(3,:),'k'), grid on
+title('Фазовый портрет при уменьшенном M_в на 15%, U_c на 20%, U_в на 20%')
+legend('x_3(x_2)')
