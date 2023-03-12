d4ed0b7281ef9ed581b4c4a0cd88ea6ea8ef9a2b
5met4nka <pavilich99@mail.ru>
2023-03-12 21:38:29 +0300

    графики выводим в одном окне

--- lab3/main.m
+++ lab3/main.m
@@ -1,18 +1,43 @@
-figure
-plot(out.Fi.Data,out.i_G.Data);
-grid on
-title('i_Г(Ф)')
-xlabel('Ф')
+figure('name', 'M=1 R=1');
+
+subplot(2,3,1);
+plot(out.tout, out.Fi.Data);
+grid on;
+grid minor;
+xlabel('t');
+ylabel('Fi');
+
+subplot(2,3,2);
+plot(out.tout, out.i_G.Data);
+xlabel('t');
+ylabel('i_G');
+grid on;
+grid minor;
+
+subplot(2,3,3);
+plot(out.tout, out.w.Data);
+grid on;
+grid minor;
+xlabel('t');
+ylabel('w');
+
+subplot(2,3,4);
+plot(out.Fi.Data, out.i_G.Data);
+grid on;
+grid minor;
+xlabel('Fi')
 ylabel('i_Г')
-figure
-plot(out.w.Data,out.i_G.Data);
-grid on
-title('i_Г(w)')
+
+subplot(2,3,5);
+plot(out.w.Data, out.i_G.Data);
+grid on; 
+grid minor;
 xlabel('w')
 ylabel('i_Г')
-figure
-plot(out.Fi.Data,out.w.Data);
-grid on
-title('w(Ф)')
-xlabel('Ф')
+
+subplot(2,3,6);
+plot(out.Fi.Data, out.w.Data);
+grid on; 
+grid minor;
+xlabel('Fi')
 ylabel('w')
