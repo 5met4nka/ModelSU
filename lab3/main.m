92ba1269848d3efa11e899dd6b29ed1449456c69
5met4nka <pavilich99@mail.ru>
2023-03-12 16:27:27 +0300

    вкинул модель и скрипты для ее запуска к лр3

--- lab3/main.m
+++ lab3/main.m
@@ -1,121 +1,18 @@
-clc, clear
-t = 0:0.01:1;
-u = [ones(size(t)); ones(size(t)); ones(size(t))];
-x0 = [0;0;0];
-[y,x] = nsim('fun_F', 'fun_G', t, u, x0, 'eiler');
-plot(t,x(1,:), 'k--', t, x(2,:), 'k', t, x(3,:), 'k:');
-grid on;
-title('График при номинальных значениях');
-legend('x_1(t)', 'x_2(t)', 'x_3(t)');
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t)); ones(size(t)); ones(size(t))];
-x0=[0;0;0];
-[y,x]=nsim('fun_F', 'fun_G', t, u, x0, 'eiler');
-plot(t,y(1,:),'k--',t,y(2,:),'k');
-grid on;
-title('График при номинальных значениях');
-legend('y_1(t)', 'y_2(t)');
-
-
-clc, clear
-t= 0:0.01:1;
-u=[ones(size(t)); ones(size(t)); ones(size(t))];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(x(2,:),x(3,:),'k');
-grid on;
-title('Фазовый портрет при номинальных значениях');
-legend('x_3(x_2)');
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t)); ones(size(t)); ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(t,x(1,:),'k--',t,x(2,:),'k',t,x(3,:),'k:'), grid on
-title('График при уменьшенном U_c на 20%')
-legend('x_1(t)','x_2(t)','x_3(t)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t));ones(size(t));ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(t,y(1,:),'k--',t,y(2,:),'k'), grid on
-title('График при уменьшенном U_c на 20%')
-legend('y_1(t)','y_2(t)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t));ones(size(t));ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(x(2,:),x(3,:),'k'), grid on
-title('Фазовый портрет при уменьшенном U_c на 20%')
-legend('x_3(x_2)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t));ones(size(t))*0.85;ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(t,x(1,:),'k--',t,x(2,:),'k',t,x(3,:),'k:'), grid on
-title('График при уменьшенном M_в на 15%, U_c на 20%')
-legend('x_1(t)','x_2(t)','x_3(t)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t));ones(size(t))*0.85;ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(t,y(1,:),'k--',t,y(2,:),'k'), grid on
-title('График при уменьшенном M_в на 15%, U_c на 20%')
-legend('y_1(t)','y_2(t)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t));ones(size(t))*0.85;ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(x(2,:),x(3,:),'k'), grid on
-title('Фазовый портрет при уменьшенном M_в на 15%, U_c на 20%')
-legend('x_3(x_2)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t))*0.8;ones(size(t))*0.85;ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(t,x(1,:),'k--',t,x(2,:),'k',t,x(3,:),'k:'), grid on
-title('График при уменьшенном M_в на 15%, U_c на 20%, U_в на 20%')
-legend('x_1(t)','x_2(t)','x_3(t)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t))*0.8;ones(size(t))*0.85;ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(t,y(1,:),'k--',t,y(2,:),'k'), grid on
-title('График при уменьшенном M_в на 15%, U_c на 20%, U_в на 20%')
-legend('y_1(t)','y_2(t)')
-
-
-clc, clear
-t=0:0.01:1;
-u=[ones(size(t))*0.8;ones(size(t))*0.85;ones(size(t))*0.8];
-x0=[0;0;0];
-[y,x]=nsim('fun_F','fun_G',t,u,x0,'eiler');
-plot(x(2,:),x(3,:),'k'), grid on
-title('Фазовый портрет при уменьшенном M_в на 15%, U_c на 20%, U_в на 20%')
-legend('x_3(x_2)')
+figure
+plot(out.Fi.Data,out.i_G.Data);
+grid on
+title('i_Г(Ф)')
+xlabel('Ф')
+ylabel('i_Г')
+figure
+plot(out.w.Data,out.i_G.Data);
+grid on
+title('i_Г(w)')
+xlabel('w')
+ylabel('i_Г')
+figure
+plot(out.Fi.Data,out.w.Data);
+grid on
+title('w(Ф)')
+xlabel('Ф')
+ylabel('w')
